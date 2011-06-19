9710004c9e0d33c3c229b022f420ab8d4d02226d
mwesterg <mwesterg@83e89a13-d7ff-4fff-b021-40a13f624d75>
2011-06-19 11:17:32 +0000

    Added license
    git-svn-id: https://svn.win.tue.nl/repos/cpntools/Simulator/trunk@192 83e89a13-d7ff-4fff-b021-40a13f624d75

---
+++ license/notice.java
@@ -0,0 +1,19 @@
+/************************************************************************/
+/* CPN Tools                                                            */
+/* Copyright 2010-##LAST## AIS Group, Eindhoven University of Technology    */
+/*                                                                      */
+/* This file is part of CPN Tools.                                      */
+/*                                                                      */
+/* CPN Tools is free software: you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 2 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* CPN Tools is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with CPN Tools.  If not, see <http://www.gnu.org/licenses/>.   */
+/************************************************************************/
